964d0a2b7904eb1a1da9bac39236c2d28f44a01c
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-05 11:19:43 +0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client"
+
+createRoot(document.getElementById("root")).render(
+    <ul>
+        <li>Super popular JS library</li>
+        <li>Will help me be even more eployable</li>
+        <li>Reace has a cool logo</li>
+    </ul>
+)
