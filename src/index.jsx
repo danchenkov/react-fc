1c2d4db81184a7d2fca1205655e47030fab102cb
Alexei Danchenkov <adanchenkov@emailline.net>
2025-01-11 15:00:00 +0300

    pre React state

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
-import { createRoot } from "react-dom/client"
+import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById("root")).render(
-    <ul>
-        <li>Super popular JS library</li>
-        <li>Will help me be even more eployable</li>
-        <li>Reace has a cool logo</li>
-    </ul>
+const root = createRoot(document.getElementById('root'))
+root.render(
+    <h1>Anything you want</h1>
 )
+
